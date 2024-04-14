8c1f9a0ddb29e896d763d88cca16ad32c91f2953
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-14 22:39:38 +0300

    postgres added

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,10 +10,10 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     def __str__(self):
-        return f"User('{self.username}', '{self.email}', '{self.password}')"
+        return f"User('{self.username}', '{self.email}', '{self.password}', '{self.image_file}')"
